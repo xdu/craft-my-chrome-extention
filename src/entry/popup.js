7486bd87a827f9435cc12e85dfd4c09abdf530cf
nasvel <du.xiaoming86@gmail.com>
2022-02-22 18:02:51 +0100

    build a vue action for fetch a feed

--- src/entry/popup.js
+++ src/entry/popup.js
@@ -12,6 +12,35 @@ const store = createStore({
     mutations: {
         increment(state) {
             state.count ++
+        },
+        feed(state, list) {
+            console.log(JSON.stringify(list))
+        }
+    },
+    actions:{
+        fetchFeed(context) {
+            fetch('https://rss.dw.com/atom/rss-chi-all')
+                .then(resp => resp.text())
+                .then(xml => {
+                    const parser = new DOMParser()
+                    const doc = parser.parseFromString(xml, 'text/xml')
+                    const list = []
+
+                    const entries = doc.getElementsByTagName('entry')
+                    for(let i = 0; i < entries.length; i ++) {
+
+                        const entry = entries[i]
+                        const json = {}
+
+                        json.id = entry.getElementsByTagName('id')[0].firstChild.nodeValue
+                        json.title = entry.getElementsByTagName('title')[0].firstChild.nodeValue
+                        json.summary = entry.getElementsByTagName('summary')[0].firstChild.nodeValue
+
+                        list.push(json)
+                    }
+
+                    context.commit('feed', list)
+                })
         }
     }
 })
