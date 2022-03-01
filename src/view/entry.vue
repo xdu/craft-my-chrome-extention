e9f91f79d2bf58af1b2748b515d3a1935006e9e4
xdu <du.xiaoming86@gmail.com>
2022-03-01 13:52:43 +0100

    left arrow key to go back

--- src/view/entry.vue
+++ src/view/entry.vue
@@ -5,30 +5,46 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
   name: "entry",
 
-  computed: {
+  mounted() {
+    window.addEventListener("keydown", this.keypress);
+  },
 
+  unmounted() {
+    window.removeEventListener("keydown", this.keypress);
+  },
+
+  computed: {
     id() {
-      return this.$route.params.id
+      return this.$route.params.id;
     },
 
     content() {
-      return this.entries.find( entry => entry.id = this.id )
+      return this.entries.find((entry) => (entry.id = this.id));
     },
 
     ...mapState({
-      entries: (state) => state.entries
-    })
+      entries: (state) => state.entries,
+    }),
   },
 
   methods: {
     back() {
-      this.$router.back()
-    }
-  }
-}
+      this.$router.back();
+    },
+
+    keypress(e) {
+      if (e.defaultPrevented) {
+        return; // Should do nothing if the default action has been cancelled
+      }
+      if (e.code === "ArrowLeft") {
+        this.back();
+      }
+    },
+  },
+};
 </script>
